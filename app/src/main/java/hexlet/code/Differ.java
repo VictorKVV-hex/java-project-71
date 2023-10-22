5fde16f5da3e5e215fd00c260a3f7fbdb18e0935
Victor KVV <kvv_post@mail.ru>
2023-10-22 22:18:47 +0300

    intermediate works

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,14 @@
 package hexlet.code;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Differ {
     public static String generate(String str1, String Str2, String formatName) {
+        str1 = "/mnt/d/VICTOR/HEXLET/java-project-71";
+        Path testFilePath = Paths.get(str1);
+        Path fileName = testFilePath.getFileName();
+        System.out.println(fileName);
         return null;
     }
 }
