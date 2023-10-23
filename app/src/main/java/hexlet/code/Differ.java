2631520415ae2f499202966713e6fcccb5279142
Victor KVV <kvv_post@mail.ru>
2023-10-23 22:57:04 +0300

    made getMap()

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -9,31 +9,33 @@ import java.nio.file.Paths;
 import java.util.Map;
 
 public class Differ {
-    public static String generate(String str1, String Str2, String formatName) throws Exception {
-/*        str1 = "/mnt/d/VICTOR/HEXLET/java-project-71";
-        Path testFilePath = Paths.get(str1);
-        Path fileName = testFilePath.getFileName();
-        System.out.println(fileName);*/
-        //        String str1 = "/mnt/d/VICTOR/HEXLET/java-project-71/file1.json";
-        String FilePath1 = "/home/kvv/PROJECTS/java-project-71/file1.json";
+    public static String generate(String filePath1, String filePath2, String formatName) throws Exception {
+//        String str1 = "/mnt/d/VICTOR/HEXLET/java-project-71/file1.json";
+//        String filePath1 = "/home/kvv/PROJECTS/java-project-71/file1.json";
         // Получаем путь к нужному файлу
-        Path testFilePath = Paths.get(FilePath1);
-        Path fileName = testFilePath.getFileName();
+        Map<String, Object> map1 = getMap(filePath1);
+        Map<String, Object> map2 = getMap(filePath2);
 
+        return "!";
+    }
+
+    public static Map<String, Object> getMap(String FilePath) throws Exception {
+        Path testFilePath = Paths.get(FilePath);
+        Path fileName = testFilePath.getFileName();
         // Формируем абсолютный путь, если filePath будет содержать относительный путь, то мы всегда будет работать с абсолютным
-        Path path = Paths.get(FilePath1).toAbsolutePath().normalize();
+        Path path = Paths.get(FilePath).toAbsolutePath().normalize();
         // Проверяем существование файла
         if (!Files.exists(path)) {
             throw new Exception("File '" + path + "' does not exist");
         }
         // Читаем файл
         String content = Files.readString(path);
-        // Выводим содержимое
-        System.out.println(content);
+//        System.out.println(content);
 /*        List<String> lines = Files.readAllLines(testFilePath, UTF_8);
         System.out.println(lines);*/
         ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> map = objectMapper.readValue(content, new TypeReference<Map<String,Object>>(){});
-        return "!";
+        Map<String, Object> map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {
+        });
+        return map;
     }
 }
