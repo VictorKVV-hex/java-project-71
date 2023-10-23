a3916c9924c84e77412a28dc606466fa127a5760
KVV <kvvpost@gmail.com>
2023-10-23 12:42:45 +0300

    intermediate works1

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,14 +1,39 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class Differ {
-    public static String generate(String str1, String Str2, String formatName) {
-        str1 = "/mnt/d/VICTOR/HEXLET/java-project-71";
+    public static String generate(String str1, String Str2, String formatName) throws Exception {
+/*        str1 = "/mnt/d/VICTOR/HEXLET/java-project-71";
         Path testFilePath = Paths.get(str1);
         Path fileName = testFilePath.getFileName();
-        System.out.println(fileName);
-        return null;
+        System.out.println(fileName);*/
+        //        String str1 = "/mnt/d/VICTOR/HEXLET/java-project-71/file1.json";
+        String FilePath1 = "/home/kvv/PROJECTS/java-project-71/file1.json";
+        // Получаем путь к нужному файлу
+        Path testFilePath = Paths.get(FilePath1);
+        Path fileName = testFilePath.getFileName();
+
+        // Формируем абсолютный путь, если filePath будет содержать относительный путь, то мы всегда будет работать с абсолютным
+        Path path = Paths.get(FilePath1).toAbsolutePath().normalize();
+        // Проверяем существование файла
+        if (!Files.exists(path)) {
+            throw new Exception("File '" + path + "' does not exist");
+        }
+        // Читаем файл
+        String content = Files.readString(path);
+        // Выводим содержимое
+        System.out.println(content);
+/*        List<String> lines = Files.readAllLines(testFilePath, UTF_8);
+        System.out.println(lines);*/
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = objectMapper.readValue(content, new TypeReference<Map<String,Object>>(){});
+        return "!";
     }
 }
