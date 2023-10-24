e45f40e7e37f57b96f550c3fe3d62fde3de0c1e2
KVV <kvvpost@gmail.com>
2023-10-24 19:45:37 +0300

    intermediate works3

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -3,19 +3,26 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.*;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2, String formatName) throws Exception {
 //        String str1 = "/mnt/d/VICTOR/HEXLET/java-project-71/file1.json";
 //        String filePath1 = "/home/kvv/PROJECTS/java-project-71/file1.json";
-        // Получаем путь к нужному файлу
         Map<String, Object> map1 = getMap(filePath1);
         Map<String, Object> map2 = getMap(filePath2);
-
+        Set<String> allKeys = new TreeSet<>();
+//        Set<String> allKeys = new LinkedHashSet<>();
+        allKeys.addAll(map1.keySet());
+        allKeys.addAll(map2.keySet());
+        LinkedHashMap<String, String> UnsortedResult =new LinkedHashMap<>();
+        Map<String, Object> mapAll = new HashMap<>(map1);
+        mapAll.putAll(map2);
+        mapAll.forEach((k2, v2) -> UnsortedResult.put(k2, Differ.valueOfMap(map1, map2, k2, v2)));
         return "!";
     }
 
@@ -30,12 +37,29 @@ public class Differ {
         }
         // Читаем файл
         String content = Files.readString(path);
-//        System.out.println(content);
-/*        List<String> lines = Files.readAllLines(testFilePath, UTF_8);
-        System.out.println(lines);*/
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, Object> map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {
         });
         return map;
+    }
+
+    public static String valueOfMap(Map<String, Object> data1, Map<String, Object> data2, String key, Object value) {
+        String Ret = null;
+        boolean isEquals = false;
+        boolean isKey1Exists = data1.containsKey(key);
+        boolean isKey2Exists = data2.containsKey(key);
+        if (isKey1Exists && isKey2Exists) {
+            isEquals = data1.get(key).equals(data2.get(key));
+        }
+        if (!isKey1Exists && isKey2Exists) {
+            Ret = "added";
+        } else if (isKey1Exists && !isKey2Exists) {
+            Ret = "deleted";
+        } else if (isKey1Exists && isKey2Exists && !isEquals) {
+            Ret = "changed";
+        } else if (isKey1Exists && isKey2Exists && isEquals) {
+            Ret = "unchanged";
+        }
+        return Ret;
     }
 }
