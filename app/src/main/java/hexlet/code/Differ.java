2a9e40eb0e57c2f0ecf5a259047e5cd026e18051
Victor KVV <kvv_post@mail.ru>
2023-11-04 13:33:35 +0300

    add testGetPath()1

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,4 @@
 package hexlet.code;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.util.ArrayList;
 import java.util.List;
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -5,21 +5,22 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.io.IOException;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parser(String content, String extension) throws JsonProcessingException, IllegalArgumentException {
-
+    public static Map<String, Object> parser(String content, String extension)
+            throws JsonProcessingException, IllegalArgumentException {
         ObjectMapper objectMapper;
         Map<String, Object> map;
 
         if (extension.equals("yml")) {
             objectMapper = new YAMLMapper();
-            map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {});
+            map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {
+            });
         } else if (extension.equals("json")) {
             objectMapper = new ObjectMapper();
-            map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {});
+            map = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {
+            });
         } else {
             throw new IllegalArgumentException("Wrong extension");
         }
