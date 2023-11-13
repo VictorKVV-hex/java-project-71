9820647d1466e99aadd8d13d9a788813cc9f043a
Victor KVV <kvv_post@mail.ru>
2023-11-13 23:50:27 +0300

    add Json, attempt add Node

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -1,6 +1,9 @@
 package hexlet.code.formatters;
 
+import hexlet.code.Node;
+
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Json {
     static List<String> diffList = new ArrayList<>();
@@ -22,14 +25,15 @@ public class Json {
     public static void differOfMap(Map<String, Object> data1, Map<String, Object> data2, String key) {
         Object valueMap1 = data1.get(key);
         Object valueMap2 = data2.get(key);
-/*        String valOne = typeValue(data1.get(key));
-        String valTwo = typeValue(data2.get(key));*/
+        String valOne = typeValue(data1.get(key));
+        String valTwo = typeValue(data2.get(key));
         StringBuilder resultStr = null;
         if (data1.containsKey(key) && data2.containsKey(key)) {
             if (Objects.equals(valueMap1, valueMap2)) {
                 diffList.add("{\"type\":\"UNCHANGED\", \"key \"" + key + "\":" + valueMap2 + "}");
             } else {
                 diffList.add("{\"type\":\"UPDATED\", \"key \"" + key + "\":" + valueMap1 + ",\"updatedValue\":" + valueMap2 + "}");
+//                diffList.add(String.valueOf(new Node("UPDATED", key, valueMap1, valueMap2)));
             }
         } else {
             if (!(data1.containsKey(key))) {
@@ -38,5 +42,20 @@ public class Json {
                 diffList.add("{\"type\":\"REMOVED\", \"key \"" + key + "\":" + valueMap1 + "}");
             }
         }
+    }
+
+    private static String typeValue(Object object) {
+        String value = String.valueOf(object);
+        if (value.contains("[") || value.contains("{")) {
+            ArrayList<String> list = (ArrayList<String>) object;
+            return list
+                    .stream()
+                    .filter(o -> o instanceof String)
+                    .map(name -> "\"" + name + "\"")
+                    .collect(Collectors.joining(","));
+        } else if (object instanceof String) {
+            return "'" + value + "'";
+        }
+        return value;
     }
 }
