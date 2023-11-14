6c70e5ea3d6c8c48c56e51f19043071423af3ca2
KVV <kvvpost@gmail.com>
2023-11-14 19:59:27 +0300

    add Json, attempt add Node #1

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -11,50 +11,61 @@ public class Json {
     public static String json(Set<String> allKeys, Map<String, Object> map1, Map<String,
             Object> map2, String extension) {
         diffList.clear();
-        if (extension.equals("json")) {
-            diffList.add("{");
-        }
         for (String key : allKeys) {
             differOfMap(map1, map2, key);
         }
-        if (extension.equals("json")) {
-            diffList.add("}");
-        }
-        return String.join("\n", diffList);
+        return "{\n" + String.join(",\n", diffList) + "\n}";
     }
     public static void differOfMap(Map<String, Object> data1, Map<String, Object> data2, String key) {
         Object valueMap1 = data1.get(key);
         Object valueMap2 = data2.get(key);
         String valOne = typeValue(data1.get(key));
         String valTwo = typeValue(data2.get(key));
-        StringBuilder resultStr = null;
+//        StringBuilder resultStr = null;
         if (data1.containsKey(key) && data2.containsKey(key)) {
             if (Objects.equals(valueMap1, valueMap2)) {
-                diffList.add("{\"type\":\"UNCHANGED\", \"key \"" + key + "\":" + valueMap2 + "}");
+                diffList.add("{\"type\":\"UNCHANGED\", \"key\":\"" + key + "\", \"value\":" + valTwo + "}");
             } else {
-                diffList.add("{\"type\":\"UPDATED\", \"key \"" + key + "\":" + valueMap1 + ",\"updatedValue\":" + valueMap2 + "}");
+                diffList.add("{\"type\":\"UPDATED\", \"key\":\"" + key + "\", \"value\":" + valOne + ", \"updatedValue\":" + valTwo + "}");
 //                diffList.add(String.valueOf(new Node("UPDATED", key, valueMap1, valueMap2)));
             }
         } else {
             if (!(data1.containsKey(key))) {
-                diffList.add("{\"type\":\"ADDED\", \"key \"" + key + "\":" + valueMap2 + "}");
+                diffList.add("{\"type\":\"ADDED\", \"key\":\"" + key + "\", \"value\":" + valTwo + "}");
             } else {
-                diffList.add("{\"type\":\"REMOVED\", \"key \"" + key + "\":" + valueMap1 + "}");
+                diffList.add("{\"type\":\"REMOVED\", \"key\":\"" + key + "\", \"value\":" + valOne + "}");
             }
         }
     }
 
     private static String typeValue(Object object) {
         String value = String.valueOf(object);
-        if (value.contains("[") || value.contains("{")) {
+        if (value.contains("[")) {
+            if (object instanceof Map<?,?>) {
+                return  value;
+            }
             ArrayList<String> list = (ArrayList<String>) object;
-            return list
-                    .stream()
-                    .filter(o -> o instanceof String)
-                    .map(name -> "\"" + name + "\"")
-                    .collect(Collectors.joining(","));
+            if (list.get(0) instanceof String) {
+                return "[" + list
+                        .stream()
+                        .map(name -> "\"" + name + "\"")
+                        .collect(Collectors.joining(",")) + "]";
+            }
+        } else if (value.contains("{")) {
+            if (object instanceof Map<?,?>) {
+                return  value;
+            }
+            ArrayList<String> list = (ArrayList<String>) object;
+            if (list.get(0) instanceof String) {
+                return "{" + list
+                        .stream()
+                        .map(name -> "\"" + name + "\"")
+                        .collect(Collectors.joining(",")) + "}";
+            }
         } else if (object instanceof String) {
-            return "'" + value + "'";
+            return  "\"" + value + "\"";
+        } else {
+            return  value;
         }
         return value;
     }
