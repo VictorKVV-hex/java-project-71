a27f76c5272e7de3a7f0740c1c07798ff687ede8
Victor KVV <kvv_post@mail.ru>
2023-11-16 22:28:51 +0300

    add generate(f1, f2, stylish)

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -11,6 +11,10 @@ import static hexlet.code.Formatter.formatter;
 import static hexlet.code.Parser.parser;
 
 public class Differ {
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        return generate(filePath1, filePath2, "stylish");
+    }
+
     public static String generate(String filePath1, String filePath2, String formatName) throws Exception {
         String extension = getExtension(filePath1);
         Map<String, Object> map1 = getMap(filePath1, extension);
