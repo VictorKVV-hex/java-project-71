41a1c28babec4e6444c9ffed90e4ed8d0795b5e9
KVV <kvvpost@gmail.com>
2023-11-24 18:00:46 +0300

    Start doing new diffList

---
+++ app/src/main/java/hexlet/code/DescriptionDiff.java
@@ -0,0 +1,20 @@
+/*
+package hexlet.code;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class DescriptionDiff {
+    public static List<Node>  getDifferList(Set<String> allKeys, Map<String, Object> map1, Map<String,
+            Object> map2, String extension) {
+        List<String> diffList = new ArrayList<>();
+        diffList.clear();
+        for (String key : allKeys) {
+            differOfMap(diffList, map1, map2, key);
+        }
+        return "[" + String.join(",", diffList) + "]";
+    }
+}
+*/
