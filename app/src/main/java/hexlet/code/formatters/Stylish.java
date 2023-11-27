75dad826917b1e24b70c6eb7d23df3b611df9c34
KVV <kvvpost@gmail.com>
2023-11-27 19:42:58 +0300

    Trying to fix check#2 on class Three Diff7

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -3,8 +3,6 @@ package hexlet.code.formatters;
 import hexlet.code.Node;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 public class Stylish {
 
